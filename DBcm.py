f2f70b24f57da0408942061912f15e8f3fd12c85
Tymoteusz Frankiewicz <krokodylfred@gmail.com>
2019-02-15 21:10:56 +0100

    Made slight progress on saving items to db

---
+++ DBcm.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+
+# poniższe wyjątki służą do odseparowania obsługi wyjątków związanych
+# z bazą danych od głównej warstwy aplikacji
+class ConnectionError(Exception):
+    pass
+
+
+class CredentialsError(Exception):
+    pass
+
+
+class SQLError(Exception):
+    pass
+
+
+class UseDatabase:
+
+    def __init__(self, config: dict) -> None:
+        """inicjalizacja menadzera kontekstu"""
+        self.configuration = config
+
+    def __enter__(self) -> 'cursor':
+        """"konfiguracja mk, to sie dzieje przed uruchomieniem zestawu instrukcji with"""
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise (ConnectionError(err))
+        except mysql.connector.errors.ProgrammingError as err:
+            raise (CredentialsError(err))
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        """kod demontujacy menadzer kontekstu"""
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise (SQLError(exc_type))
+        elif exc_type:
+            raise exc_type(exc_val)
--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from DBcm import UseDatabase
+
+import mysql.connector
 
 app = Flask(__name__)
 
+# baza: homeBrainDB
+# identyfikator; homeBrain
+# hasło: #W4lepsze
+# tabela: bilance
+
+dbconfig = {'host': '127.0.0.1',
+            'user': 'homeBrain',
+            'password': '#W4lepsze',
+            'database': 'homeBrainDB'}
+
+
+def bilance_requst(req: 'flask_request'):
+    with UseDatabase(dbconfig) as cursor:
+        _SQL = """insert into bilance
+                (category, name, value, date)
+                values 
+                (%s, %s, %d, %s)"""
+    cursor.execute(_SQL, req.form['select-type'], req.form['name'], req.form['value'], req.form['date'])
 
 @app.route('/')
 def index():
+    # bilance_requst()
     return render_template('index.html')
 
 
