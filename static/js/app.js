2512efb737c7dcf35edbfedc9f22330f9bb348d2
Szymon Wiśniewski <szymwisn@gmail.com>
2019-02-26 15:47:12 +0100

    Added box to edit data

--- static/js/app.js
+++ static/js/app.js
@@ -12,13 +12,19 @@ const UIcontroller = (function () {
         btnAdd: '.btn-add',
         btnSmallMenu: '#ico-small-menu',
         smallMenuContent: '#small-menu',
-        btnSmallMenuClose: '#btn-small-menu-close'
+        btnSmallMenuClose: '#btn-small-menu-close',
+        btnEditItem: '.btn-edit-item',
+        btnCloseEdit: '.btn-close-edit',
+        modalEdit: '#modal-edit',
+        expensesBox: '#box-expenses',
+        incomeBox: '#box-income'
     };
 
     return {
         setInitialDate: function() {
             document.querySelector(DOMstrings.inputItemDate).valueAsDate = new Date();
         },
+
 
         getDOMstrings: function() {
             return DOMstrings;
@@ -32,16 +38,20 @@ const UIcontroller = (function () {
 const controller = (function (dataCtrl, UICtrl) {
     let DOM;
 
+
     function toggleSmallMenu() {
         document.querySelector(DOM.smallMenuContent).classList.toggle('visible');
     }
 
+
     function setUpDOM() {
         DOM = UICtrl.getDOMstrings();
 
+        // enter and close hamburger menu
         document.querySelector(DOM.btnSmallMenu).addEventListener('click', toggleSmallMenu);
         document.querySelector(DOM.btnSmallMenuClose).addEventListener('click', toggleSmallMenu);
 
+        // change input border-color depending on item type - income/expense
         document.querySelector(DOM.selectItemType).addEventListener('change', () => {
             const inputs = document.querySelectorAll('.input');
             const arrInputs = Array.from(inputs);
@@ -49,6 +59,26 @@ const controller = (function (dataCtrl, UICtrl) {
             arrInputs.forEach(current => {
                 current.classList.toggle('input-income');
             })
+        });
+
+        // open editing box
+        document.querySelector(DOM.expensesBox).addEventListener('click', event => {
+            if(event.target.className === 'btn btn-edit')
+                document.querySelector(DOM.modalEdit).style.display = 'block';
+        });
+
+        document.querySelector(DOM.incomeBox).addEventListener('click', event => {
+            if(event.target.className === 'btn btn-edit')
+                document.querySelector(DOM.modalEdit).style.display = 'block';
+        });
+
+        // close the editing box
+        document.querySelector(DOM.btnEditItem).addEventListener('click', () => {
+            document.querySelector(DOM.modalEdit).style.display = 'none';
+        });
+
+        document.querySelector(DOM.btnCloseEdit).addEventListener('click', () => {
+            document.querySelector(DOM.modalEdit).style.display = 'none';
         });
     }
 
