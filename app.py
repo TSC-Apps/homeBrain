3aea08f5f46fcfd2f0d64f197aec8dc6d2594cd2
Szymon Wiśniewski <szymwisn@gmail.com>
2019-02-28 13:48:21 +0100

    Added 404 page

--- app.py
+++ app.py
@@ -152,5 +152,12 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
