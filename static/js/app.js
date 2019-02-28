7c058ed7a72da2782102d3fe033d3afc311bcef1
Szymon Wiśniewski <szymwisn@gmail.com>
2019-02-28 12:57:40 +0100

    Added JS to month and year select

--- static/js/app.js
+++ static/js/app.js
@@ -17,12 +17,54 @@ const UIcontroller = (function () {
         btnCloseEdit: '.btn-close-edit',
         modalEdit: '#modal-edit',
         expensesBox: '#box-expenses',
-        incomeBox: '#box-income'
+        incomeBox: '#box-income',
+        selectMonths: '.select-months',
+        selectYears: '.select-years',
+        selectAcceptDate: '.select-accept-date'
     };
 
     return {
         setInitialDate: function() {
-            document.querySelector(DOMstrings.inputItemDate).valueAsDate = new Date();
+            let now, month, year, months, years, selectMonths, selectYears, optionMonths, optionYears;
+
+            now = new Date();
+            month = now.getMonth();
+            year = now.getFullYear();
+            months = ['STY', 'LUT', 'MAR', 'KWI', 'MAJ', 'CZE', 'LIP', 'SIE', 'WRZ', 'PAZ', 'LIS', 'GRU'];
+            years = [];
+
+            // set input date to today
+            document.querySelector(DOMstrings.inputItemDate).valueAsDate = now;
+
+            for(let i = 2018; i <= year; i++) {
+                years.push(i);
+            }
+
+            // fill in months select
+            selectMonths = document.querySelector(DOMstrings.selectMonths);
+            optionMonths = [];
+
+            for(let i = 0; i < months.length; i++) {
+                optionMonths[i] = document.createElement("option");
+                optionMonths[i].text = months[i];
+                optionMonths[i].value = i + 1;
+                selectMonths.add(optionMonths[i]);
+            }
+
+            // fill in years select
+            selectYears = document.querySelector(DOMstrings.selectYears);
+            optionYears = [];
+
+            for(let i = 0; i < years.length; i++) {
+                optionYears[i] = document.createElement("option");
+                optionYears[i].text = years[i];
+                optionYears[i].value = years[i];
+                selectYears.add(optionYears[i]);
+            }
+
+            // set initial select values
+            selectMonths.selectedIndex = month;
+            selectYears.selectedIndex = year - 2018;
         },
 
 
@@ -82,10 +124,14 @@ const controller = (function (dataCtrl, UICtrl) {
         });
     }
 
+    function setUpDate() {
+        UICtrl.setInitialDate();
+    }
+
     return {
         initialize: function() {
             setUpDOM();
-            UICtrl.setInitialDate();
+            setUpDate();
         },
     };
 })(dataController, UIcontroller);
